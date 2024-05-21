2024c77a4e70893c5ade731a5d586681ddc44be6
shivasai <149556451+shivasai8688@users.noreply.github.com>
2024-05-21 18:18:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,23 @@
-asgmin = "1"
-asgmax = "2"
-asgDesired = "2"
+variable "Machinetype" {
+  description = "Machine type"
+  type = string
+  default = "t2.micro"
+}
+
+variable "keyname" {
+  description = "Machine Key"
+  type = string
+  default = "vv"
+}
+
+variable "InstanceZone" {
+  description = "Zone of the Instance"
+  type = string
+  default = "ap-south-1"
+}
+
+variable "VolumeSize" {
+  description = "Size of the Machine"
+  type = number
+  default = 1
+}
